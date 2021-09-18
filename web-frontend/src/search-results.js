b8dbec49dbb0cd09df5940c26c8407c4421a46b5
Lacico <Lacicocodes@gmail.com>
2021-09-18 15:22:24 +0100

    Trimmed some of the defaults from the web app

--- web-frontend/src/search-results.js
+++ web-frontend/src/search-results.js
@@ -6,16 +6,13 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(title, buyer, keywords, date, link) {
+  return { title, buyer, keywords, date, link };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('title 1', 'buyer 1', "science, ai, data", '20210730T083000', 'https://demo1.com'),
+  createData('title 2', 'buyer 2', 'data', '20210729T223000', 'https://demo2.com'),
 ];
 
 export default function BasicTable() {
@@ -24,11 +21,11 @@ export default function BasicTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Title</TableCell>
+            <TableCell align="right">Buyer</TableCell>
+            <TableCell align="right">Keywords Matched</TableCell>
+            <TableCell align="right">Date</TableCell>
+            <TableCell align="right">link</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -38,12 +35,12 @@ export default function BasicTable() {
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.title}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.buyer}</TableCell>
+              <TableCell align="right">{row.keywords}</TableCell>
+              <TableCell align="right">{row.date}</TableCell>
+              <TableCell align="right">{row.link}</TableCell>
             </TableRow>
           ))}
         </TableBody>
